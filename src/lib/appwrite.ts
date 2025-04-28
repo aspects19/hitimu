0d1cb2fa59e5775f500b82f89924aa9494b3cadf
aspect19 <jeffamenya388@gmail.com>
2025-04-29 01:43:11 +0300

    MVP ui

--- src/lib/appwrite.ts
+++ src/lib/appwrite.ts
@@ -1,9 +1,11 @@
+import 'dotenv/config'
 import { Client, Account} from 'appwrite';
+
 
 export const client = new Client();
 
 client
-.setEndpoint('https://fra.cloud.appwrite.io/v1')
+.setEndpoint('https://fra.cloud.appwrite.io/v1' ) 
 .setProject('6809ec0600316ea99779');
 
 export const account = new Account(client);
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,5 +1,9 @@
 import { createFileRoute } from '@tanstack/react-router'
 import { useState } from 'react';
+import CountUp from 'react-countup';
+import { PiStudentFill } from "react-icons/pi";
+import { FaUniversity } from "react-icons/fa";
+import { ImBooks } from "react-icons/im";
 
 export const Route = createFileRoute('/')({
   component: Index,
@@ -58,35 +62,51 @@ function Index() {
         Lecture notes, past papers, and more! <br/>
         <span className="text-xl font-normal">All in one place</span>
       </p>
-      <label className="input mt-10">
-        <svg className="h-[1em] opacity-50" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
-          <g
-            strokeLinejoin="round"
-            strokeLinecap="round"
-            strokeWidth="2.5"
-            fill="none"
-            stroke="currentColor"
-          >
-            <circle cx="11" cy="11" r="8"></circle>
-            <path d="m21 21-4.3-4.3"></path>
-          </g>
-        </svg>
-        <input type="search" required placeholder="Search" />
-      </label>
+      <div className="flex flex-col md:flex-row items-center mt-5">
+        <label className="input mt-10">
+          <svg className="h-[1em] opacity-50" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+            <g
+              strokeLinejoin="round"
+              strokeLinecap="round"
+              strokeWidth="2.5"
+              fill="none"
+              stroke="currentColor"
+            >
+              <circle cx="11" cy="11" r="8"></circle>
+              <path d="m21 21-4.3-4.3"></path>
+            </g>
+          </svg>
+          <input type="search" required placeholder="Search" />
+        </label>
+        <button className="btn w-full mx-[-7px] md:w-16 md:mt-10 md:ml-[-7px] btn-primary mt-2">Search</button>
+      </div>
       <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 mt-10'>
-        <div className="card bg-base-100 w-96 shadow-sm">
-          
-          <div className="card-body items-center text-center">
-            <h2 className="card-title text-center">Card Title</h2>
-            <p>A card component has a figure, a body part, and inside body there are title and actions parts</p>
-            <div className="card-actions">
-              <button className="text-xl hover:font-bold ml-auto mr-0">{'>'}</button>
-            </div>
+        
+        <div className=" flex items-center stat card bg-base-100 shadow-sm">
+          <FaUniversity className="text-5xl text-primary" />
+          <div className="stat-value text-primary">
+            <CountUp end={500} duration={5} />
           </div>
+          <h1 className="font-bold text-primary-content">Schools available</h1>
+        </div>
+
+        <div className=" flex items-center stat card bg-base-100 shadow-sm">
+          <PiStudentFill className="text-5xl text-primary" />
+          <div className="stat-value text-primary">
+            <CountUp end={100} duration={5} />
+          </div>
+          <h1 className="font-bold text-primary-content">Students Joined</h1>
+        </div>
+
+        <div className=" flex items-center stat card bg-base-100 shadow-sm">
+          <ImBooks className="text-5xl text-primary" />
+          <div className="stat-value text-primary">
+            <CountUp end={2000} duration={8} />
+          </div>
+          <h1 className="font-bold text-primary-content">Material available</h1>
         </div>
         
       </div>
-        
     </div>
   );
 };
