ef43689136e986642dbf4cf93aa484efc9569b09
aspect19 <jeffamenya388@gmail.com>
2025-04-29 09:23:45 +0300

    Relatively complete ui

--- src/routes/(tabs)/profile.tsx
+++ src/routes/(tabs)/profile.tsx
@@ -20,16 +20,29 @@ function RouteComponent() {
     <span className='text-base mt-1'>Student</span>
     <span className='text-xl font-bold mt-4'>Your Uploads</span>
     <div className='flex w-full flex-row gap-5 overflow-x-auto whitespace-nowrap mt-4 max-w-72 '>
-      <div className="card w-48 border-white border-1 h-40">Computer Science</div>
-      <div className="card w-48 border-white border-1 h-40">Computer Science</div>
-      <div className="card w-48 border-white border-1 h-40">Computer Science</div>
-      <div className="card w-48 border-white border-1 h-40">Computer Science</div>
-      <div className="card w-48 border-white border-1 h-40">Computer Science</div>
-      <div className="card w-48 border-white border-1 h-40">Computer Science</div>
+      <div className="card  skeleton w-48 border-white border-1 h-40">Computer Science</div>
+      <div className="card skeleton w-48 border-white border-1 h-40">Computer Science</div>
+      <div className="card skeleton  w-48 border-white border-1 h-40">Computer Science</div>
+      <div className="card skeleton w-48 border-white border-1 h-40">Computer Science</div>
+      <div className="card skeleton w-48 border-white border-1 h-40">Computer Science</div>
+      <div className="card skeleton w-48 border-white border-1 h-40">Computer Science</div>
     </div>
-    <div className='flex flex-col '>
+    <span className="text-xl font-bold mt-10 mb-2 block">Recently Viewed</span>
+    <div className="flex gap-4 overflow-x-auto">
+      <div className="card w-48 h-32 bg-base-200"></div>
+      <div className="card w-48 h-32 bg-base-200"></div>
+      <div className="card w-48 h-32 bg-base-200"></div>
+    </div>
 
+    <span className="text-xl font-bold mt-10 mb-2 block">Recommended</span>
+    <div className="flex gap-4 overflow-x-auto mb-10">
+      <div className="card w-48 h-32 bg-base-200"></div>
+      <div className="card w-48 h-32 bg-base-200"></div>
+      <div className="card w-48 h-32 bg-base-200"></div>
     </div>
+
+
+    
   </div>
   );
 };
--- src/routes/upload.tsx
+++ src/routes/upload.tsx
@@ -11,10 +11,10 @@ function RouteComponent() {
         <h2 className="text-2xl font-bold mb-4 text-center">Upload Study Material</h2>
         
         <form className='flex flex-col items-center' onSubmit={()=> {}}>
-          <input type="file" className="file-input file-input-bordered h-28 mb-3" required />
           <input type="text" placeholder="Title" className="input input-bordered mb-3" required />
           <input type="text" placeholder="Subject" className="input input-bordered mb-3" required />
           <textarea placeholder="Description (optional)" className="textarea textarea-bordered mb-3"></textarea>
+          <input type="file" className="file-input file-input-bordered mb-3" required />
           <button type="submit" className="btn btn-primary w-full">Upload</button>
         </form>
       </div>
